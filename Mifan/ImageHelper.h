e0e5a4d236782c421836871463b40b0dd03506e4
ryan715 <763914974@qq.com>
2014-04-14 10:30:36 +0800

    用户详情

---
+++ Mifan/ImageHelper.h
@@ -0,0 +1,13 @@
+//
+//  ImageHelper.h
+//  Mifan
+//
+//  Created by ryan on 14-4-11.
+//  Copyright (c) 2014年 ryan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageHelper : NSObject
+
+@end
---
+++ Mifan/ImageHelper.m
@@ -0,0 +1,13 @@
+//
+//  ImageHelper.m
+//  Mifan
+//
+//  Created by ryan on 14-4-11.
+//  Copyright (c) 2014年 ryan. All rights reserved.
+//
+
+#import "ImageHelper.h"
+
+@implementation ImageHelper
+
+@end
